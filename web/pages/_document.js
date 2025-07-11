8b78111502070fc02dfe579e4e2dba3668fe3c07
Nope Lavanda <nopelavanda@Nopes-MacBook-Air.local>
2025-07-11 21:32:01 +0700

    feat(web): add default meta title and favicon to _document.js

---
+++ web/pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>LocalTrip - You Travel We Capture the Story</title>
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
